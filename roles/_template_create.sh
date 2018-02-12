3805e26d5ecb3180a13a72eee7088e83a2c52beb
Adrien Mahieux <adrien@mahieux.net>
2018-02-12 01:50:21 +0100

    [Enh][template] Add pre and packages

---
+++ roles/_template_create.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+typeset MYSELF="$(readlink -f $0)"
+typeset MYPATH="${MYSELF%/*}"
+
+
+typeset ROLENAME="$1"
+typeset SRCDIR="$MYPATH/_template"
+typeset DSTDIR="$MYPATH/$ROLENAME"
+
+[[ -z "$ROLENAME" ]] && {
+	echo "Usage: $0 'rolename'"
+	echo "  Rolename being the new role located in $MYPATH"
+	exit 1
+}
+[[ -e "$DSTDIR" ]] && {
+	echo "Role '$ROLENAME' already exists"
+	exit 1
+}
+echo $ROLENAME | grep -qP '[\./#]' && {
+	echo "rolename cannot contain '.' or '/'"
+	exit 1
+}
+
+# Copy files
+echo "Creating '$ROLENAME' as '$DSTDIR'"
+rsync -a "$SRCDIR/" "$DSTDIR"
+
+# Sed the templates
+typeset tplreplace="${ROLENAME#*-}"
+typeset tplsearch="__TEMPLATE__"
+grep -r "$tplsearch" -l | xargs sed -e "s/$tplsearch/$tplreplace/g" -i
+
+
